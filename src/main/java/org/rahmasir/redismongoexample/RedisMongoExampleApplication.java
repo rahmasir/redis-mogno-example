db770f203bd2d1e39123cd42ee8c2e24e30bf192
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-11 11:55:00 +0330

    update: adding file structures - blank spring boot project

---
+++ src/main/java/org/rahmasir/redismongoexample/RedisMongoExampleApplication.java
@@ -0,0 +1,13 @@
+package org.rahmasir.redismongoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMongoExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisMongoExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rahmasir/redismongoexample/RedisMongoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rahmasir.redismongoexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisMongoExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
